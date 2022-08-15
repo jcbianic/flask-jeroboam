3b6c4bbd81a97e497a5980b4cfa68b8d9b866d01
Jean-Christophe Bianic <75325590+jcbianic@users.noreply.github.com>
2022-08-15 14:34:34 +0200

    :construction: Basic request parser decorator

---
+++ tests/conftest.py
@@ -0,0 +1,133 @@
+"""Configuration File for pytest."""
+import json
+import os
+
+import pytest
+from flask import Response
+from pydantic import BaseModel
+
+from flask_jeroboam.jeroboam import Jeroboam
+
+
+@pytest.fixture
+def app() -> Jeroboam:
+    app = Jeroboam("jeroboam_test", root_path=os.path.dirname(__file__))
+    app.config.update(
+        TESTING=True,
+        SECRET_KEY="test key",
+    )
+    return app
+
+
+@pytest.fixture
+def app_ctx(app: Jeroboam):
+    with app.app_context() as ctx:
+        yield ctx
+
+
+@pytest.fixture
+def request_context(app: Jeroboam):
+    with app.test_request_context() as ctx:
+        yield ctx
+
+
+@pytest.fixture
+def client(app: Jeroboam):
+    return app.test_client()
+
+
+@pytest.fixture(scope="session")
+def reponse_model_test_class():
+    """Basic Response Model."""
+
+    class TestResponseModel(BaseModel):
+        data_str: str
+        data_int: int
+
+    return TestResponseModel
+
+
+@pytest.fixture(scope="session")
+def inbound_model_test_class():
+    """Basic Response Model."""
+
+    class InBoundModel(BaseModel):
+        data_str: str
+        data_int: int
+
+    return InBoundModel
+
+
+@pytest.fixture(scope="session")
+def valid_inbound_dict():
+    """Basic Inbound Dictionnary."""
+    return {"data_str": "test", "data_int": 1}
+
+
+@pytest.fixture(scope="session")
+def dict_endpoint(valid_inbound_dict):
+    """Endpoint-Like function returning a dict."""
+
+    def endpoint():
+        return valid_inbound_dict
+
+    return endpoint
+
+
+@pytest.fixture(scope="session")
+def model_endpoint(reponse_model_test_class, valid_inbound_dict):
+    """Endpoint-Like function returning a Pydantic Model."""
+
+    def endpoint():
+        return reponse_model_test_class(**valid_inbound_dict)
+
+    return endpoint
+
+
+@pytest.fixture(scope="session")
+def response_endpoint():
+    """Endpoint-Like function returning a Flask Response."""
+
+    def endpoint():
+        return Response(
+            json.dumps({"emails": "test"}),
+            mimetype="application/json",
+            status=200,
+        )
+
+    return endpoint
+
+
+@pytest.fixture(scope="session")
+def endpoint_with_params(inbound_model_test_class):
+    """Endpoint-Like function returning a Flask Response."""
+
+    def endpoint(id: int, other_param: str, query: inbound_model_test_class):
+        return query
+
+    return endpoint
+
+
+@pytest.fixture(scope="session")
+def endpoint_with_simple_annotations(inbound_model_test_class):
+    """Endpoint-Like function returning a Flask Response."""
+
+    def endpoint(id: int, other_param: str):
+        return {"id": id, "other_param": other_param}
+
+    return endpoint
+
+
+@pytest.fixture
+def test_app():
+    """Test Flask App."""
+    
+
+    app = Jeroboam(__name__)
+
+    @app.route("/")
+    def endpoint(id: int, other_param: str):
+        return {"id": id, "other_param": other_param}
+    
+    with app.test_client() as client:
+        yield client
