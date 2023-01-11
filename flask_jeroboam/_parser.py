806361565dc3c0877ccf4a5f6ee2d71b6b092803
Jean-Christophe Bianic <jc.bianic@gmail.com>
2023-01-11 11:35:55 +0100

    :ok_hand: Add new locations, optional params and list params to Request Parsing

--- flask_jeroboam/_parser.py
+++ flask_jeroboam/_parser.py
@@ -1,4 +1,5 @@
 import json
+import re
 import typing as t
 from enum import Enum
 from functools import wraps
@@ -13,6 +14,7 @@ from flask_jeroboam.exceptions import InvalidRequest
 F = t.TypeVar("F", bound=t.Callable[..., t.Any])
 R = t.TypeVar("R", bound=t.Any)
 P = ParamSpec("P")
+T = t.TypeVar("T", bound=t.Any)
 
 
 class MethodEnum(str, Enum):
@@ -25,33 +27,67 @@ class MethodEnum(str, Enum):
     PATCH = "PATCH"
 
 
+pattern = r"(.*)\[(.+)\]$"
+
+
 def _parse_input(model: t.Type[BaseModel], **kwargs: ParamSpec) -> BaseModel:
     try:
         return model(**kwargs)
     except ValueError as e:
-        raise InvalidRequest(msg_to_user=str(e), context=str(kwargs)) from e
+        raise InvalidRequest(msg=str(e)) from e
 
 
-def parser(method: MethodEnum):
+def _simple_parse_input(type_: T, payload: dict, key: str) -> T:
+    try:
+        return type_(payload.get(key, None))
+    except ValueError as e:
+        raise InvalidRequest(msg=str(e)) from e
+
+
+def _rename_keys(location: dict, pattern: str) -> dict:
+    renamings = []
+    for key, value in location.items():
+        match = re.match(pattern, key)
+        if len(value) == 1 and match is None:
+            location[key] = value[0]
+        elif match is not None:
+            new_key = match.group(1) + "[]"
+            new_value = {match.group(2): value[0]}
+            renamings.append((key, new_key, new_value))
+    for key, new_key, new_value in renamings:
+        if new_key not in location:
+            location[new_key] = [new_value]
+        else:
+            location[new_key].append(new_value)
+        del location[key]
+    return location
+
+
+def parser(
+    method: str, rule: str
+) -> t.Callable[[t.Callable[..., R]], t.Callable[..., R]]:
     """Parametrize Decorator for parsing request data."""
 
-    def parser_decorator(func: t.Callable[P, R]) -> t.Callable[P, R]:
+    def parser_decorator(func: t.Callable[..., R]) -> t.Callable[..., R]:
         @wraps(func)
         def wrapper(*args, **kwargs):
             if method == MethodEnum.GET:
-                location = request.args
+                location = dict(request.args.lists())
+                location = _rename_keys(location, pattern)
             elif method == MethodEnum.POST:
-                data = dict(request.form)
-                if request.content_type == "application/json":
-                    data.update(json.loads(request.data))
-                else:
-                    data.update(dict(request.data))
-                location = data
+                location = dict(request.form)
+                if request.data:
+                    location.update(dict(json.loads(request.data)))
+                location.update(dict(request.files))  # type: ignore
             else:
                 location = {}
             for arg_name, arg_type in func.__annotations__.items():
+                if getattr(arg_type, "__origin__", None) == t.Union:
+                    arg_type = arg_type.__args__[0]
                 if issubclass(arg_type, BaseModel):
                     kwargs[arg_name] = _parse_input(arg_type, **location)
+                elif arg_name not in rule:
+                    kwargs[arg_name] = _simple_parse_input(arg_type, location, arg_name)
             return func(*args, **kwargs)
 
         return wrapper
