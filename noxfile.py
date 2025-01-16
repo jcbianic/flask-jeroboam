0b05d70979e5e498c7d22a5354f9786d55fa9ca7
Jean-Christophe Bianic <jcbianic@libon.com>
2025-01-16 15:24:33 +0100

    Narrowing the Python Versions

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 """Nox sessions."""
+
 import os
 import shlex
 import shutil
@@ -23,7 +24,7 @@ except ImportError:
 
 
 package = "flask_jeroboam"
-python_versions = ["3.11", "3.10", "3.9", "3.8"]
+python_versions = ["3.11", "3.10", "3.9"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
