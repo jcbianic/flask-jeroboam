d6ecafd1588029b729f086a21b5ee6caebcd0bcf
Jean-Christophe Bianic <jcbianic@libon.com>
2025-01-24 22:35:13 +0100

    :ok_hand: Tweak the project.toml file to be compatible up to python 3.13 and remove Safety

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ except ImportError:
 
 
 package = "flask_jeroboam"
-python_versions = ["3.11", "3.10", "3.9"]
+python_versions = ["3.13", "3.12", "3.11", "3.10", "3.9"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
@@ -33,7 +33,6 @@ nox.options.sessions = (
     "typeguard",
     "xdoctest",
     "docs-build",
-    "safety",
 )
 
 
